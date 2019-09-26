aa4c6c3cafcc2f3dfcc365fec7e6986c9a50e9ec
Gabor Szabo <gabor.szabo86@hotmail.com>
2019-09-26 02:31:07 +0200

    Post route added, body-parser

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var campgrounds = [
+    { name: 'Salmon Creek', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
+    { name: 'Granite Hill', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
+    { name: 'Mountain Goat\'s Rest', image: 'https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' }
+];
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    var campgrounds = [
-        { name: 'Salmon Creek', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'},
-        { name: 'Granite Hill', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'},
-        { name: 'Mountain Goat\'s Rest', image: 'https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}
-    ]
+
     res.render('campgrounds', {camps: campgrounds});
+});
+
+app.post('/campgrounds', (req, res) => {
+    // ! get data from a form and add it to the array
+    var name = req.body.name;
+    var image = req.body.image;
+    var obj = {name: name, image: image};
+    campgrounds.push(obj);
+    res.redirect('/campgrounds');
+    // todo: redirect back to campgrounds page
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new')
 });
 
 app.listen(4000, () => {
