4b25a26b7b8b5dd428e53935f109c4c4255c644a
Gabor Szabo <gabor.szabo86@hotmail.com>
2019-10-04 20:07:15 +0200

    Campgrounds detail view added, changes, refactoring

--- app.js
+++ app.js
@@ -1,43 +1,60 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const db = require('./config/keys').mongoURI;
+
+const Campground = require('./models/camps');
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
-var campgrounds = [
-    { name: 'Salmon Creek', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Granite Hill', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Mountain Goat\'s Rest', image: 'https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Salmon Creek', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Granite Hill', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Mountain Goat\'s Rest', image: 'https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Salmon Creek', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Granite Hill', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' },
-    { name: 'Mountain Goat\'s Rest', image: 'https://images.pexels.com/photos/2419278/pexels-photo-2419278.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' }
-];
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
+// Render camps from database 
 app.get('/campgrounds', (req, res) => {
-
-    res.render('campgrounds', {camps: campgrounds});
+    Campground.find({})
+    .then(allCampgrounds => {
+        res.render('index', {camps: allCampgrounds});
+    })
+    .catch(err => {
+        console.log(err);
+    })
 });
 
+// Save camps to database
 app.post('/campgrounds', (req, res) => {
-    // ! get data from a form and add it to the array
-    var name = req.body.name;
-    var image = req.body.image;
-    var obj = {name: name, image: image};
-    campgrounds.push(obj);
+    const {name, image, description} = req.body;
+    var newCampground = new Campground({
+        name,
+        image,
+        description
+    });
+
+    newCampground.save();
     res.redirect('/campgrounds');
-    // todo: redirect back to campgrounds page
 });
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new')
+});
+
+app.get('/campgrounds/:id', (req, res) => {
+    // find the campground with the provided id
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('show', {campground: foundCampground});
+        }
+    })
 });
 
 app.listen(4000, () => {
---
+++ models/camps.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const campgroundSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+module.exports = Campground;
